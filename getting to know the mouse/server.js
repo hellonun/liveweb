3cf86396257ad3a0a62b73a5925dacbd9ff59da3
hellonun <43077909+hellonun@users.noreply.github.com>
2019-10-17 09:56:38 -0400

    Add files via upload

---
+++ getting to know the mouse/server.js
@@ -0,0 +1,91 @@
+// HTTP Portion
+var http = require('http');
+var fs = require('fs'); // Using the filesystem module
+var httpServer = http.createServer(requestHandler);
+var url = require('url');
+
+var id = [];
+var idp = [];
+var total = [];
+var totals = [];
+
+function requestHandler(req, res) {
+
+	var parsedUrl = url.parse(req.url);
+	console.log("The Request is: " + parsedUrl.pathname);
+
+	// Read in the file they requested
+	fs.readFile(__dirname + parsedUrl.pathname,
+		// Callback function for reading
+		function (err, data) {
+			// if there is an error
+			if (err) {
+				res.writeHead(500);
+				return res.end('Error loading ' + parsedUrl.pathname);
+			}
+			// Otherwise, send the data, the contents of the file
+			res.writeHead(200);
+			res.end(data);
+		}
+	);
+}
+
+// Call the createServer method, passing in an anonymous callback function that will be called when a request is made
+var httpServer = http.createServer(requestHandler);
+
+// Tell that server to listen on port 8080
+httpServer.listen(8080);
+console.log('Server listening on port 8080');
+
+// WebSocket Portion
+// WebSockets work with the HTTP server
+var io = require('socket.io').listen(httpServer);
+
+
+// Register a callback function to run when we have an individual connection
+// This is run for each individual user that connects
+io.sockets.on('connection',
+// We are given a websocket object in our function
+
+function (socket) {
+
+	console.log("We have a new client: " + socket.id);
+	idp.push ([socket.id]);
+
+	socket.on('name', function(name) {
+		console.log(name);
+		if (name == "nun") {
+			console.log("name correct");
+		}
+	});
+
+	socket.on('xvalue', function(x) {
+		io.sockets.emit("xvalue", x);
+		// console.log(x);
+	});
+
+	socket.on('position', function(data) {
+		// check if id is already there, if yes, delete and push new position
+		for (i=0;i<idp.length; i++){
+			if (socket.id == idp[i][0]){
+				idp.splice(i,1);
+				idp.push ([socket.id, data]);
+			}
+		}
+
+		// console.log (idp);
+		io.sockets.emit("position", idp);
+	});
+
+
+	socket.on('disconnect', function() {
+		// check if id closed window, if yes, delete from array
+		console.log("Client has disconnected: " + socket.id);
+		for (i=0;i<idp.length; i++){
+			if (socket.id == idp[i][0]) {
+				idp.splice(i,1);
+			}
+		}
+	});
+}
+);
---
+++ getting to know the mouse/sketch.js
@@ -0,0 +1,12 @@
+// var canvas;
+//
+// function setup() {
+//   canvas = createCanvas(windowWidth, windowHeight);
+//   canvas.position (0,0);
+//   canvas.style('z-index', '-1');
+//   background (0,255,0);
+// }
+//
+// function draw() {
+//
+// }
